289f4265344ad3389447ea0dc545ec4335df7dca
TromhiesGIG <vicekid46@gmail.com>
2025-03-27 02:00:04 -0400

    Set WHITENOISE_MANIFEST_STRICT to False in settings.py

--- Scheduler/settings.py
+++ Scheduler/settings.py
@@ -119,6 +119,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 # Ensure Whitenoise can serve static files in production
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# WHITENOISE_MANIFEST_STRICT = True
+WHITENOISE_MANIFEST_STRICT = False
 
 # Login/Logout Path
 
